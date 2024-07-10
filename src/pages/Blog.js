7758a0fbdcf487d21d57a83c40b2827bb67251b9
Nino-YM <119847747+Nino-YM@users.noreply.github.com>
2024-07-10 12:12:26 +0200

    fin du tutoriel

---
+++ src/pages/Blog.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+const Blog = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>Bienvenu sur mon blog</h1>
+        </div>
+    );
+};
+export default Blog;
